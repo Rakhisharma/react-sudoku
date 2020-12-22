c8479f51abe4643bec06c8de789d559e12c37eaf
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-22 19:05:24 +0100

    Add grid styled and tsx componenet

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+const Grid: FC = () => {
+  return (
+    <div data-cy="grid-container">
+      <div data-cy="grid-row-container">
+        <div data-cy="block"> </div>
+      </div>
+    </div>
+  )
+}
+
+export default Grid
