4894462fb7201dd7e763a26bf5b2dfadbf96b375
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-22 20:09:48 +0100

    Add container and row compoenent style

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,24 +1,27 @@
 import React, { Children, FC } from 'react'
+import { Container, Row } from './styles'
 
 const Grid: FC = () => {
   return (
-    <div data-cy="grid-container">
+    <Container data-cy="grid-container">
       {/* Use Children.toArray() to map items, as in this way you
         you don't have to pass 'key' or 'id' in your div. if you use just array
         you need to pass key in you element, react provides a way to get rid of this 
         using Children.toArray()*/}
       {Children.toArray(
         [...Array(9)].map((_, rowIndex) => (
-          <div data-cy="grid-row-container">
+          <Row data-cy="grid-row-container">
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <div data-cy="block" key={colIndex}></div>
+                <div data-cy="block" key={colIndex}>
+                  Test
+                </div>
               ))
             )}
-          </div>
+          </Row>
         ))
       )}
-    </div>
+    </Container>
   )
 }
 
