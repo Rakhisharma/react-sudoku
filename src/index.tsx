9ac7478806468f47fde768d71caaae2c80263937
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-22 17:58:26 +0100

    Use theme for styling and extend theme default type using ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
 import { unregister } from './core'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   // <> is also called Fragment. you can use <Fragment> instead of <>
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <div>HelloWorld</div>
-  </>,
+  </ThemeProvider>,
   document.getElementById('root')
 )
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,26 +1,24 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 export default createGlobalStyle`
-html {
-    height: 100%;
-
-    body {
+  ${({ theme }) => css`
+    html {
+      height: 100%;
+      body {
         display: flex;
         flex-direction: column;
         height: 100%;
         margin: 0;
-
         #root {
-            /* 282c34 is a color and adding cc at the end 
-            i.e '282c34cc' gives it opacity attribute */
-            background: radial-gradient(#282c34cc, #282c34);
-            color: #282c34;
-            display: flex;
-            font-family: sans-serif;
-            height: 100%;
-            justify-content: center;
-            padding: 15px;
+          background: ${theme.colors.background};
+          color: ${theme.colors.black};
+          display: flex;
+          font-family: sans-serif;
+          height: 100%;
+          justify-content: center;
+          padding: 15px;
         }
+      }
     }
-}
+  `}
 `
