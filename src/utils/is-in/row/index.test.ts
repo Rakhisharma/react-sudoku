5c7db0c9a01c8225d2c647d8eaeb10fd99763db4
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-27 16:06:45 +0100

    add unti test for isInCol

---
+++ src/utils/is-in/row/index.test.ts
@@ -0,0 +1,7 @@
+import isInRow from './index'
+
+describe('isInRow', () => {
+  it('returns true when value is in grid row', () => {})
+
+  it('returns false when value is in grid row', () => {})
+})
