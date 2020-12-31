4e2bef6f6d86a22d6d7692286e94f98b1bb45410
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-31 15:01:48 +0100

    useSelctor to map grid value to each individula cell

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
 
+import { IReducer } from 'reducers'
 import { Container } from './styles'
+import { N } from 'Typings'
 interface Props {
   colIndex: number
   rowIndex: number
 }
 
+interface IState {
+  value: N
+}
+
 const Block: FC<Props> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
+  const state = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][colIndex] : 0,
+  }))
+  return (
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+      {state.value}
+    </Container>
+  )
 }
 
 export default Block
