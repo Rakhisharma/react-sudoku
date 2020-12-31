0f8d1ba13d13ce880010c7167dc2d4260289b8be
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2020-12-31 13:50:52 +0100

    setup redux

---
+++ src/reducers/reducer.ts
@@ -0,0 +1,11 @@
+import { AnyAction } from 'redux'
+const intialState = {}
+
+const reducer = (state: typeof intialState, action: AnyAction) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
