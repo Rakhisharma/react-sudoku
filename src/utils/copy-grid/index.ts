905923ab94fb53b9288f2eadbb856ba5fe95e3e8
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-01-02 14:31:55 +0100

    Implement aopy grid function

---
+++ src/utils/copy-grid/index.ts
@@ -0,0 +1,22 @@
+import { GRID, INDEX } from 'Typings'
+
+const copyGrid = (grid: GRID): GRID => {
+  const gridCopy: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+
+  for (let r: INDEX = 0; r < 0; r++)
+    for (let c: INDEX = 0; c < 0; c++) gridCopy[r][c] = grid[r][c]
+
+  return gridCopy
+}
+
+export default copyGrid
