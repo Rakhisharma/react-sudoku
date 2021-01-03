50036616ec0e890d405339a18b247b2d9a3ec3e4
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-01-03 16:29:23 +0100

    map number button

--- src/components/numbers/index.tsx
+++ src/components/numbers/index.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
 
 import { NUMBERS } from 'Typings'
-import { Container } from './styles'
+
 import Button from './button'
+import { Container } from './styles'
 
 const Numbers: FC = () => (
   <Container>
-    Numbers
     {([1, 2, 3, 4, 5, 6, 7, 8, 9] as NUMBERS[]).map((value) => (
       <Button key={value} value={value} />
     ))}
